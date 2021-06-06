406c9bc1161c89dccaa04cb72249552db34739b4
Kyle Conibear <kyle.a.conibear@gmail.com>
2021-06-06 11:57:37 -0400

    Basic 8 LED strip

--- Arduino_LED-Track-Puzzle/Arduino_LED-Track-Puzzle.ino
+++ Arduino_LED-Track-Puzzle/Arduino_LED-Track-Puzzle.ino
@@ -1,9 +1,39 @@
+#include <FastLED.h>
+
+
+// How many leds are in the strip?
+#define NUM_LEDS 8
+
+
+// For led chips like WS2812, which have a data line, ground, and power, you just
+// need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
+// ground, and power), like the LPD8806 define both DATA_PIN and CLOCK_PIN
+// Clock pin only needed for SPI based chipsets when not using hardware SPI
+#define DATA_PIN 3
+
+// This is an array of leds.  One item for each led in your strip.
+CRGB leds[NUM_LEDS];
+
 void setup() {
   // put your setup code here, to run once:
-
+  FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);  // GRB ordering is typical
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  
+  // Move a single white led 
+   for(int whiteLed = 0; whiteLed < NUM_LEDS; whiteLed = whiteLed + 1) {
+      // Turn our current led on to white, then show the leds
+      leds[whiteLed] = CRGB::White;
 
+      // Show the leds (only one of which is set to white, from above)
+      FastLED.show();
+
+      // Wait a little bit
+      delay(100);
+
+      // Turn our current led back to black for the next loop around
+      leds[whiteLed] = CRGB::Black;
+   }
 }
